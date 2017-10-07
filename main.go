fd106e2ddc0d155e8f05a1e77a1d418a1cee9977
Christopher Reid <creid@iloan.com>
2017-10-07 11:21:02 -0500

    Added healthcheck

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	srss := SoundcloudRSSServer{}
+	http.HandleFunc("/monitoring/healthcheck", healthcheck)
 	http.Handle("/", &srss)
 	http.ListenAndServe(":8080", nil)
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	status := http.StatusOK
+	w.WriteHeader(status)
+	fmt.Fprintf(w, "%s\n", http.StatusText(status))
 }
